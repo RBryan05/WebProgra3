3a8dc25f07790db9f5f7dcfee0789a8898f49034
Alexis Rauda <126432796+RBryan05@users.noreply.github.com>
2025-05-06 22:56:28 -0600

    Cambios en el enlace de la API

--- AppPaginaWeb/api.py
+++ AppPaginaWeb/api.py
@@ -1,7 +1,7 @@
 import requests
 
 # URLs base de la API
-BASE_URL = "http://127.0.0.1:3000/api/"
+BASE_URL = "https://apiproyectoprogra3.onrender.com/api/"
 ENDPOINTS = {
     "usuarios": "usuarios/",
     "negocios": "negocios/",
